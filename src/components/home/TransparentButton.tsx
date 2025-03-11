240f3e50c864a78e2ac8965cd72ac26de3a07afd
Abdullah Usman <ib6457345@gmail.com>
2025-03-11 17:31:54 +0500

    border change

--- src/components/home/TransparentButton.tsx
+++ src/components/home/TransparentButton.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link'
 import React from 'react'
 import { CircleArrow } from '../shared';
 
-export default function TransparentButton({ href = "/",text ,textColor= "white",backgroundColor="#ED1E24"}: { href?: string,text:string,textColor?:string ,backgroundColor?:string}) {
+export default function TransparentButton({ href = "/",text ,textColor= "white"}: { href?: string,text:string,textColor?:string }) {
   return (
     <Link 
       href={href} 
-      className={`group flex items-center justify-between gap-4 border-2 border-[${backgroundColor}] hover:text-white hover:bg-[#ED1E24] text-${textColor} pl-6 pr-1 py-1 rounded-full transition`}
+      className={`group flex items-center justify-between gap-4 border-2 border-[#ED1E24] hover:text-white hover:bg-[#ED1E24] text-${textColor} pl-6 pr-1 py-1 rounded-full transition`}
     >
       {text}
       <CircleArrow/>
